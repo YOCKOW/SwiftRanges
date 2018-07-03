482c91d27bce7a9806247289dac5fabafd2d60d8
YOCKOW <YOCKOW@users.noreply.github.com>
2018-07-03 16:38:08 +0900

    Add overlap tests: LeftOpenRange vs LeftOpenRange

--- Tests/RangesTests/OverlapTests.swift
+++ Tests/RangesTests/OverlapTests.swift
@@ -120,6 +120,110 @@ final class OverlapTests: XCTestCase {
     }
   }
   
+  func test_LeftOpenRange_LeftOpenRange_Int() {
+    let cases:[(LeftOpenRange<Int>, Bool)] = [
+      (10<..15, false),
+      (10<..20, false),
+      (10<..21, true),
+      (10<..30, true),
+      (10<..39, true),
+      (10<..40, true),
+      (10<..41, true),
+      (10<..50, true),
+      
+      (19<..20, false),
+      (19<..21, true),
+      (19<..30, true),
+      (19<..39, true),
+      (19<..40, true),
+      (19<..41, true),
+      (19<..50, true),
+      
+      (20<..20, false),
+      (20<..21, true),
+      (20<..30, true),
+      (20<..39, true),
+      (20<..40, true),
+      (20<..41, true),
+      (20<..50, true),
+      
+      (30<..30, false),
+      (30<..39, true),
+      (30<..40, true),
+      (30<..41, true),
+      (30<..50, true),
+      
+      (39<..39, false),
+      (39<..40, true),
+      (39<..41, true),
+      (39<..50, true),
+      
+      (40<..40, false),
+      (40<..41, false),
+      (40<..50, false),
+      
+      (41<..41, false),
+      (41<..50, false),
+    ]
+    
+    for (range, expected) in cases {
+      let message = "\(leftOpenRange_Int) vs \(range): Overlaps?: `\(expected)` is expected."
+      XCTAssertEqual(leftOpenRange_Int.overlaps(range), expected, message)
+    }
+  }
+  
+  func test_LeftOpenRange_LeftOpenRange_Double() {
+    let cases:[(LeftOpenRange<Double>, Bool)] = [
+      (1.0<..1.5, false),
+      (1.0<..2.0, false),
+      (1.0<..2.1, true),
+      (1.0<..3.0, true),
+      (1.0<..3.9, true),
+      (1.0<..4.0, true),
+      (1.0<..4.1, true),
+      (1.0<..5.0, true),
+      
+      (1.9<..2.0, false),
+      (1.9<..2.1, true),
+      (1.9<..3.0, true),
+      (1.9<..3.9, true),
+      (1.9<..4.0, true),
+      (1.9<..4.1, true),
+      (1.9<..5.0, true),
+      
+      (2.0<..2.0, false),
+      (2.0<..2.1, true),
+      (2.0<..3.0, true),
+      (2.0<..3.9, true),
+      (2.0<..4.0, true),
+      (2.0<..4.1, true),
+      (2.0<..5.0, true),
+      
+      (3.0<..3.0, false),
+      (3.0<..3.9, true),
+      (3.0<..4.0, true),
+      (3.0<..4.1, true),
+      (3.0<..5.0, true),
+      
+      (3.9<..3.9, false),
+      (3.9<..4.0, true),
+      (3.9<..4.1, true),
+      (3.9<..5.0, true),
+      
+      (4.0<..4.0, false),
+      (4.0<..4.1, false),
+      (4.0<..5.0, false),
+      
+      (4.1<..4.1, false),
+      (4.1<..5.0, false),
+    ]
+    
+    for (range, expected) in cases {
+      let message = "\(leftOpenRange_Double) vs \(range): Overlaps?: `\(expected)` is expected."
+      XCTAssertEqual(leftOpenRange_Double.overlaps(range), expected, message)
+    }
+  }
+  
   func test_LeftOpenRange_OpenRange_Int() {
     let cases:[(OpenRange<Int>, Bool)] = [
       (10<.<15, false),
@@ -539,6 +643,8 @@ final class OverlapTests: XCTestCase {
   static var allTests = [
     ("test_LeftOpenRange_ClosedRange_Int", test_LeftOpenRange_ClosedRange_Int),
     ("test_LeftOpenRange_ClosedRange_Double", test_LeftOpenRange_ClosedRange_Double),
+    ("test_LeftOpenRange_LeftOpenRange_Int", test_LeftOpenRange_LeftOpenRange_Int),
+    ("test_LeftOpenRange_LeftOpenRange_Double", test_LeftOpenRange_LeftOpenRange_Double),
     ("test_LeftOpenRange_OpenRange_Int", test_LeftOpenRange_OpenRange_Int),
     ("test_LeftOpenRange_OpenRange_Double", test_LeftOpenRange_OpenRange_Double),
     ("test_LeftOpenRange_Range_Int", test_LeftOpenRange_Range_Int),
