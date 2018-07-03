32c6af8fe2ad977ffe493e2619ae5bc1d12c1831
YOCKOW <YOCKOW@users.noreply.github.com>
2018-07-03 13:35:57 +0900

    Implemented `func overlaps(_:ClosedRange)` of `OpenRange`.

--- Tests/RangesTests/OpenRangeTests.swift
+++ Tests/RangesTests/OpenRangeTests.swift
@@ -28,8 +28,44 @@ final class OpenRangeTests: XCTestCase {
     XCTAssertFalse((1.0<.<1.1).isEmpty)
   }
   
+  // `Int` conforms to `Strideable` and its `Stride` conforms to `BinaryInteger`
+  let intOR: OpenRange<Int> = 10<.<20
+  // `Double` also conforms to `Strideable` but its `Stride` doesn't confom to `BinaryInteger`
+  let doubleOR: OpenRange<Double> = 1.0<.<2.0
+  
+  func testOverlaps_ClosedRange() {
+    let intCR: [(ClosedRange<Int>, Bool)] = [
+      (0...10, false),
+      (0...11, true),
+      (0...30, true),
+      (20...30, false),
+      (30...40, false),
+    ]
+    let doubleCR: [(ClosedRange<Double>, Bool)] = [
+      (0...1.0, false),
+      (0...1.1, true),
+      (0...3.0, true),
+      (2.0...3.0, false),
+      (3.0...4.0, false),
+    ]
+    for (closedRange, expected) in intCR {
+      XCTAssertEqual(intOR.overlaps(closedRange), expected,
+                     "OpenRange:\(intOR), ClosedRange:\(closedRange)")
+      XCTAssertEqual(closedRange.overlaps(intOR), expected,
+                     "OpenRange:\(intOR), ClosedRange:\(closedRange)")
+    }
+    for (closedRange, expected) in doubleCR {
+      XCTAssertEqual(doubleOR.overlaps(closedRange), expected,
+                     "OpenRange:\(doubleOR), ClosedRange:\(closedRange)")
+      XCTAssertEqual(closedRange.overlaps(doubleOR), expected,
+                     "OpenRange:\(doubleOR), ClosedRange:\(closedRange)")
+    }
+  }
+  
   static var allTests = [
     ("testAsRangeExpression", testAsRangeExpression),
+    ("testEmptiness", testEmptiness),
+    ("testOverlaps_ClosedRange", testOverlaps_ClosedRange),
   ]
 }
 
