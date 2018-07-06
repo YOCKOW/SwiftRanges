33e7b2f6396f971f9c14e93fcc4f1d9756b39b75
YOCKOW <YOCKOW@users.noreply.github.com>
2018-07-06 10:00:47 +0900

    Add a container for ranges.

---
+++ Sources/Ranges/AnyRange.swift
@@ -0,0 +1,28 @@
+/***************************************************************************************************
+ AnyRange.swift
+   © 2018 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ **************************************************************************************************/
+
+/// # AnyRange
+/// A container for a range.
+public struct AnyRange<Bound> where Bound: Comparable {
+  internal enum _Range {
+    case empty
+    case unboundedRange
+    case closedRange(ClosedRange<Bound>)
+    case leftOpenRange(LeftOpenRange<Bound>)
+    case openRange(OpenRange<Bound>)
+    case partialRangeFrom(PartialRangeFrom<Bound>)
+    case partialRangeGreaterThan(PartialRangeGreaterThan<Bound>)
+    case partialRangeThrough(PartialRangeThrough<Bound>)
+    case partialRangeUpTo(PartialRangeUpTo<Bound>)
+  }
+  
+  private var _range: _Range
+  
+  internal init(_ range:_Range) {
+    self._range = range
+  }
+}
