fb9fc5511308d3b0b2514d102b8c9e408ad9dbf4
YOCKOW <YOCKOW@users.noreply.github.com>
2018-07-15 11:22:05 +0900

    Implement `EmptyRange`.

---
+++ Sources/Ranges/EmptyRange.swift
@@ -0,0 +1,51 @@
+/***************************************************************************************************
+ EmptyRange.swift
+   © 2018 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ **************************************************************************************************/
+
+
+/// # EmptyRange
+///
+/// A range that does not contain any elements.
+public struct EmptyRange<Bound> where Bound:Comparable {}
+
+extension EmptyRange  {
+  public var isEmpty: Bool { return true }
+}
+
+extension EmptyRange: Equatable {
+  public static func ==(lhs:EmptyRange<Bound>, rhs:EmptyRange<Bound>) -> Bool {
+    return true
+  }
+}
+
+extension EmptyRange: Hashable where Bound: Hashable {
+  public func hash(into hasher:inout Hasher) {
+    hasher.combine(0)
+  }
+}
+
+extension EmptyRange: CustomStringConvertible {
+  public var description: String {
+    return "()"
+  }
+}
+
+extension EmptyRange: RangeExpression {
+  public func contains(_ element: Bound) -> Bool {
+    return false
+  }
+  
+  public func relative<C>(to collection: C) -> Range<Bound> where C:Collection, Bound == C.Index {
+    return collection.startIndex..<collection.startIndex
+  }
+  
+}
+
+extension EmptyRange: GeneralizedRange {
+  public var bounds:Bounds<Bound>? {
+    return nil
+  }
+}
