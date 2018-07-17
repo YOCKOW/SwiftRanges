519f1eb5ddf910f1e90daf6e614b3aaffdc29656
YOCKOW <YOCKOW@users.noreply.github.com>
2018-07-17 09:44:53 +0900

    Simplify the algorithm for intersection.

--- Sources/Ranges/GeneralizedRange+Intersection.swift
+++ Sources/Ranges/GeneralizedRange+Intersection.swift
@@ -5,40 +5,18 @@
      See "LICENSE.txt" for more information.
  **************************************************************************************************/
 
-extension Boundary {
-  fileprivate func _isApartFrom(_ other:Boundary<Bound>) -> Bool {
-    // self is upper, other is lower...
-    if self.bound < other.bound { return true }
-    if self.bound > other.bound { return false }
-    
-    // From here self.bound == other.bound
-    if self.isIncluded && other.isIncluded { return false }
-    return true
-  }
-}
-
-extension AnyRange {
-  /// intersection that is independent on countability
-  fileprivate func _intersection(_ other:AnyRange<Bound>) -> AnyRange<Bound>? {
-    if self == other || other.isUnbounded { return self }
-    if self.isEmpty || other.isEmpty { return .empty }
-    if self.isUnbounded { return other }
-    
-    let (left, right) = self < other ? (self, other) : (other, self)
-    
-    let leftBounds = left.bounds!
-    let rightBounds = right.bounds!
-    
-    if let leftUpper = leftBounds.upper,
-      let rightLower = rightBounds.lower,
-      leftUpper._isApartFrom(rightLower)
-    {
-      // No overlap
-      return .empty
-    }
-    
+private func _intersection<Bound>(_ lb:Bounds<Bound>?, _ rb:Bounds<Bound>?) -> Bounds<Bound>?
+  where Bound: Comparable
+{
+  guard let leftBounds = lb, let rightBounds = rb else {
+    // When at least one of them is empty, the intersection is also empty.
     return nil
   }
+  
+  let lower = Boundary._max(leftBounds.lower, rightBounds.lower, as:.lower)
+  let upper = Boundary._min(leftBounds.upper, rightBounds.upper, as:.upper)
+  
+  return (lower:lower, upper:upper)
 }
 
 extension GeneralizedRange {
@@ -46,21 +24,13 @@ extension GeneralizedRange {
   public func intersection<R>(_ other:R) -> AnyRange<Bound>
     where R:GeneralizedRange, R.Bound == Bound, Bound:Strideable, Bound.Stride:SignedInteger
   {
-    if let range = AnyRange<Bound>(self)._intersection(AnyRange<Bound>(other)) { return range }
-    
-    let lower = Boundary<Bound>._max(self.bounds?.lower, other.bounds?.lower, as:.lower)
-    let upper = Boundary<Bound>._min(self.bounds?.upper, other.bounds?.upper, as:.upper)
-    return AnyRange(uncheckedBounds:(lower:lower, upper:upper))
+    return AnyRange(uncheckedBounds:_intersection(self.bounds, other.bounds))
   }
   
   /// Returns a new range that is an intersection of `self` and `other`.
   public func intersection<R>(_ other:R) -> AnyRange<Bound>
     where R:GeneralizedRange, R.Bound == Bound
   {
-    if let range = AnyRange<Bound>(self)._intersection(AnyRange<Bound>(other)) { return range }
-    
-    let lower = Boundary<Bound>._max(self.bounds?.lower, other.bounds?.lower, as:.lower)
-    let upper = Boundary<Bound>._min(self.bounds?.upper, other.bounds?.upper, as:.upper)
-    return AnyRange(uncheckedBounds:(lower:lower, upper:upper))
+    return AnyRange(uncheckedBounds:_intersection(self.bounds, other.bounds))
   }
 }
